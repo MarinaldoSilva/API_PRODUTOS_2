f1a20b0ed5ce80530db90a365a52882447fbba5e
Mario <marinaldo12@hotmail.com>
2025-10-01 23:39:35 -0300

    remove tests e updando admin do core para acesso via web

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Produto
 
-# Register your models here.
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['dono','nome','preco','estoque','disponivel','data_validade','data_cadastro']
+    search_fields = ['dono__username','nome','preco','estoque','disponivel','data_validade','data_cadastro']
+    
