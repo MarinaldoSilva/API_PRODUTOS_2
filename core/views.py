73391469c45a24c3eb372ee35a8751bd3b8473e0
Mario <marinaldo12@hotmail.com>
2025-10-03 14:42:18 -0300

    modificação na chamado do produtoservice, agora é feito por instancia de classe com injeção de user'reuqest.user' no service

--- core/views.py
+++ core/views.py
@@ -1,46 +1,70 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .service import ProdutoService
+from .utils import get_status_errors
 
 
 class ProdutoListCreateAPIView(APIView):
 
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        prod, erro = ProdutoService.get_all_produtos_user(request.user)
+        service = ProdutoService(user=request.user)
+        """
+        o service ao ser instanciado, já recebe o valor do request.user, e salva na variavel user no service a instânica da class, sendo assim não é mais necessário passar o valor no serializer.save(dono=user) toda vez que tiver que for salvar um produto no banco.
+        """
+        produto, erro = service.get_all_produtos_user()
         if erro:
-            return Response(erro, status=status.HTTP_400_BAD_REQUEST)
-        return Response(prod, status=status.HTTP_200_OK)
+            status_error = get_status_errors(erro)
+            return Response(erro, status=status_error)
+        return Response(produto, status=status.HTTP_200_OK)
     
     def post(self, request):
-        prod, erro = ProdutoService.create_produto(request.data, request.user)
+        service = ProdutoService(user=request.user)
+        produto, erro = service.create_produto(request.data)
         if erro:
-            return Response(erro, status=status.HTTP_400_BAD_REQUEST)
-        return Response(prod, status=status.HTTP_201_CREATED)
+            status_error = get_status_errors(erro)
+            return Response(erro, status=status_error)
+        return Response(produto, status=status.HTTP_201_CREATED)
     
 class ProdutoDetailAPIView(APIView):
 
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, pk):
-        prod, erro = ProdutoService.get_pk_produto_user(pk,request.user)
+        service = ProdutoService(user=request.user)
+        produto, erro = service.get_pk_produto_user(pk)
+
         if erro:
-            return Response(erro, status=status.HTTP_404_NOT_FOUND)
-        return Response(prod, status=status.HTTP_200_OK)
+            status_error = get_status_errors(erro)
+            return Response(erro, status=status_error)
+        return Response(produto, status=status.HTTP_200_OK)
     
     def patch(self, request, pk):
-        prod, erro = ProdutoService.update_produto_user(pk, request.data, request.user, partial=True)
+        service = ProdutoService(user=request.user)
+        produto, erro = service.update_produto_user(pk, request.data, partial=True)
         if erro:
-            return Response(erro, status=status.HTTP_404_NOT_FOUND)
-        return Response(prod, status=status.HTTP_200_OK)
+            status_error = get_status_errors(erro)
+            return Response(erro, status=status_error)
+        return Response(produto, status=status.HTTP_200_OK)
     
     def put(self, request, pk):
-        prod, erro = ProdutoService.update_produto_user(pk, request.data, request.user)
+        service = ProdutoService(user=request.user)
+        produto, erro = service.update_produto_user(pk, request.data)
         if erro:
-            return Response(erro, status=status.HTTP_404_NOT_FOUND)
-        return Response(prod, status=status.HTTP_200_OK)
+            status_error = get_status_errors(erro)
+            return Response(erro, status=status_error)
+        return Response(produto, status=status.HTTP_200_OK)
     
     def delete(self, request, pk):
-        prod, erro = ProdutoService.deletar_produto_user(pk, request.user)
+        service = ProdutoService(user=request.user)
+        produto, erro = service.deletar_produto_user(pk)
         if erro:
-            return Response(erro, status=status.HTTP_404_NOT_FOUND)
+            status_error = get_status_errors(erro)
+            return Response(erro, status=status_error)
         return Response(status=status.HTTP_204_NO_CONTENT)
